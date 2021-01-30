638b97f8a1366054fa818f8971c9aed7baf926b8
Jason <jason@casjay.net>
2021-01-29 23:28:01 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
 APPNAME="nginx-manager"
-DATADIR="/srv/docker/$APPNAME"
 
 systemctl disable -now apache2 httpd nginx >/dev/null 2>&1
 
 mkdir -p "$DATADIR"/{config,data,letsencrypt} && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker stop "$APPNAME"
-docker rm -f "$APPNAME"
-docker pull jc21/nginx-proxy-manager:2
+  docker stop "$APPNAME"
+  docker rm -f "$APPNAME"
+  docker pull jc21/nginx-proxy-manager:2
 fi
 
-if [ ! -f "/srv/docker/nginx-manager/config/config.json" ]; then
-cat <<EOF | sudo tee > /srv/docker/nginx-manager/config/config.json
+if [ ! -f "$DATADIR/config/config.json" ]; then
+  cat <<EOF | sudo tee $DATADIR/config/config.json >/dev/null 2>&1
 {
   "database": {
     "engine": "knex-native",
@@ -30,19 +29,18 @@ EOF
 fi
 
 docker run -d \
---name="$APPNAME" \
---hostname "$APPNAME" \
---restart=always \
---privileged \
--e DISABLE_IPV6=true \
--p 80:80 \
--p 443:443 \
--p 8888:81 \
--v $DATADIR/data:/data \
--v $DATADIR/letsencrypt:/etc/letsencrypt \
--v $DATADIR/config/config.json:/app/config/production.json \
-jc21/nginx-proxy-manager:2
-
+  --name="$APPNAME" \
+  --hostname "$APPNAME" \
+  --restart=always \
+  --privileged \
+  -e DISABLE_IPV6=true \
+  -p 80:80 \
+  -p 443:443 \
+  -p 8888:81 \
+  -v "$DATADIR/data":/data \
+  -v "$DATADIR/letsencrypt":/etc/letsencrypt \
+  -v $"DATADIR/config/config.json":/app/config/production.json \
+  jc21/nginx-proxy-manager:2
 
 echo "
 Email:    admin@example.com
